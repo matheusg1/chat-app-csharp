bb65e894f088d4eeba7687ea779f0002e232e286
unknown <tecmatheusg@hotmail.com>
2023-10-06 13:24:20 -0300

    Mensagens unidirecionais, com cópia p/terceiro

--- Chat.App/Hubs/ChatHub.cs
+++ Chat.App/Hubs/ChatHub.cs
@@ -1,6 +1,20 @@
-﻿namespace Chat.App.Hubs
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Chat.App.Hubs
 {
-    public class Hub
+    public class ChatHub : Hub
     {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            //await Clients.User("Usuario").SendAsync("ReceiveMessage", user, message);
+        }
+
+        public async Task SendToUser(string user, string receiverConnectionId, string message)
+        {
+            await Clients.Client(receiverConnectionId).SendAsync("ReceiveMessage", user, message);
+        }
+
+        public string GetConnectionId() => Context.ConnectionId;
     }
 }
--- Chat.App/wwwroot/js/chat.js
+++ Chat.App/wwwroot/js/chat.js
@@ -11,6 +11,9 @@ connection.on("ReceiveMessage", function (user, message) {
 });
 
 connection.start().then(function () {
+    connection.invoke("GetConnectionId").then(function (id){
+        document.getElementById("connectionId").innerText = id;
+    })
     $("#send").disabled = false;
 }).catch(function (err) {
     return console.error(err.toString());
@@ -22,5 +25,24 @@ $("#send").on("click", function (event) {
     connection.invoke("SendMessage", user, message).catch(function (err) {
         return console.error(err.toString());
     });
+    event.preventDefault();
+});
+
+$("#sendPrivate").on("click", function (event) {
+    let fromUser = $("#usuario").val();
+    let toUser = $("#usuarioDestinatario").val();
+    let message = $("#mensagem").val();
+    let admin = $("#admin").val();
+
+    connection.invoke("SendToUser", fromUser, toUser, message)
+        .then(function () {
+            connection.invoke("SendToUser", fromUser, admin, message).catch(function (err) {
+                console.error(err.toString());
+            });
+        })
+        .catch(function (err) {
+            console.error(err.toString());
+        });
+
     event.preventDefault();
 });
